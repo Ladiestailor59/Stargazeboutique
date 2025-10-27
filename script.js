3c325db483fe008bbc5cb6a4d995ad0dc09636dc
Stargazeboutique <stargazeboutique11@gmail.com>
2025-10-27 20:53:58 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', ()=>{
-  document.querySelectorAll('.view-product').forEach(btn=>{
-    btn.addEventListener('click', (e)=>{
-      const id = e.currentTarget.dataset.id;
-      window.location.href = 'product-detail.html?id='+id;
-    });
-  });
-});
